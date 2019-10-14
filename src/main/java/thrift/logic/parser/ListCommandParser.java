65be4cd430fe3a4b0ee19ce425ae8d8243d10930
Oen Qi Han Kenson <kensonoenqh@gmail.com>
2019-10-14 21:10:42 +0800

    Further edit listcommand to return exceptions when bad input or wrong prefixes are used.

--- src/main/java/thrift/logic/parser/ListCommandParser.java
+++ src/main/java/thrift/logic/parser/ListCommandParser.java
@@ -1,6 +1,7 @@
 package thrift.logic.parser;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import thrift.commons.core.Messages;
 import thrift.logic.commands.ListCommand;
@@ -17,15 +18,38 @@ public class ListCommandParser implements Parser<ListCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_MONTH ,CliSyntax.PREFIX_TAG);
+        if (args.isEmpty()) {
+            return new ListCommand(); //list all transactions
+        } else if (!argMultimap.getValue(CliSyntax.PREFIX_MONTH).isEmpty() &&
+                !argMultimap.getValue(CliSyntax.PREFIX_TAG).isEmpty()) {
+            String month = argMultimap.getValue(CliSyntax.PREFIX_MONTH).get();
+            String tag = argMultimap.getValue(CliSyntax.PREFIX_TAG).get();
+            return new ListCommand(); //list all transactions filtered by both month and tag, coming in v1.3
+        } else if (!argMultimap.getValue(CliSyntax.PREFIX_MONTH).isEmpty() &&
+                argMultimap.getValue(CliSyntax.PREFIX_TAG).isEmpty()) {
+            String month = argMultimap.getValue(CliSyntax.PREFIX_MONTH).get();
+            return new ListCommand(); //list all transactions filtered by month, coming in v1.3
+        } else if (argMultimap.getValue(CliSyntax.PREFIX_MONTH).isEmpty() &&
+                !argMultimap.getValue(CliSyntax.PREFIX_TAG).isEmpty()) {
+            String month = argMultimap.getValue(CliSyntax.PREFIX_MONTH).get();
+            return new ListCommand(); //list all transactions filtered by month, coming in v1.3
+        } else { //bad args input or wrong prefixes used will throw the parseexception
+            throw new ParseException(
+                    String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
+        }
+    }
 
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, CliSyntax.PREFIX_MONTH);
-            if (argMultimap.getValue(CliSyntax.PREFIX_MONTH).isEmpty()) {
-                return new ListCommand();
-            } else {
-                System.out.println("working with m/months");
-                throw new ParseException(
-                        String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
-            }
+    /**
+     * This methods checks if the required prefixes are present in the {@code ArgumentMultimap}.
+     *
+     * @param argumentMultimap the object to check for the existence of prefixes.
+     * @param prefixes variable amount of {@code Prefix} to confirm the existence of.
+     * @return true if specified prefixes are present in the argumentMultimap.
+     */
+    protected static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
